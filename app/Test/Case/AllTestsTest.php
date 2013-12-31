9cd994afb889a9e4af7299093a5ea8502be3f836
Travis Davis <tdavis@axiapayments.com>
2013-12-31 12:04:40 -0800

    included Controller and View in the AllTests test suite

--- app/Test/Case/AllTestsTest.php
+++ app/Test/Case/AllTestsTest.php
@@ -10,7 +10,9 @@ class AllTestsTest extends PHPUnit_Framework_TestSuite {
         $path = APP . 'Test' . DS . 'Case' . DS;
 
         $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory($path . 'Controller' . DS);
         $suite->addTestDirectory($path . 'Model' . DS);
+        $suite->addTestDirectory($path . 'View' . DS);
         return $suite;
 
     }
