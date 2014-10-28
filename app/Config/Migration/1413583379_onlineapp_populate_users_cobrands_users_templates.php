1138a55eb58a3b94432a7417603d222688837b4d
Sean Brady <sbrady@axiapayments.com>
2014-10-28 13:55:56 -0700

    [447] - new migration - populate users cobrands, users templates

---
+++ app/Config/Migration/1413583379_onlineapp_populate_users_cobrands_users_templates.php
@@ -0,0 +1,119 @@
+<?php
+class OnlineappPopulateUsersCobrandsUsersTemplates extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function before($direction) {
+		$User = ClassRegistry::init('User');
+		$Template = ClassRegistry::init('Template');
+		$UserCobrand = ClassRegistry::init('UserCobrand');
+		$UserTemplate = ClassRegistry::init('UserTemplate');
+
+		$users = $User->find(
+			'list',
+			array(
+				'fields' => array(
+					'id',
+					'template_id'
+				)
+			)
+		);
+
+		foreach ($users as $userId => $templateId) {
+			$template = $Template->find(
+				'list',
+				array(
+					'fields' => array(
+						'id',
+						'cobrand_id'
+					),
+					'conditions' => array(
+						'Template.id' => $templateId
+					)
+				)
+			);
+
+			$key = key($template);
+			$cobrandId = $template[$key];
+
+			if ($direction == 'up') {
+				$exists = $UserCobrand->find(
+					'first',
+					array (
+						'conditions' => array(
+							'user_id' => $userId,
+							'cobrand_id' => $cobrandId,
+						)
+					)
+				);
+
+				if (!$exists) {
+					$UserCobrand->create(
+						array(
+							'user_id' => $userId,
+							'cobrand_id' => $cobrandId,
+						)
+					);
+
+					$UserCobrand->save();
+				}
+
+				$exists = $UserTemplate->find(
+					'first',
+					array (
+						'conditions' => array(
+							'user_id' => $userId,
+							'template_id' => $templateId,
+						)
+					)
+				);
+
+				if (!$exists) {
+					$UserTemplate->create(
+						array(
+							'user_id' => $userId,
+							'template_id' => $templateId,
+						)
+					);
+
+					$UserTemplate->save();
+				}
+			}
+		}
+
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
