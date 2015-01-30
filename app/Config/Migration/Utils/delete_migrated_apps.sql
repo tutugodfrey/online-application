4030048e11ad73c97791d2e03cce90835fa9c542
Sean Brady <sbrady@axiapayments.com>
2015-01-30 10:29:54 -0800

    also update email timelines and coversheets... remove foreign key

--- app/Config/Migration/Utils/delete_migrated_apps.sql
+++ app/Config/Migration/Utils/delete_migrated_apps.sql
@@ -1,5 +1,17 @@
+BEGIN;
+
+UPDATE onlineapp_coversheets
+   SET cobranded_application_id = null
+ WHERE onlineapp_application_id < 5000;
+
+UPDATE onlineapp_email_timelines
+   SET cobranded_application_id = null
+ WHERE app_id < 5000;
+
 DELETE FROM onlineapp_cobranded_application_values
-      WHERE cobranded_application_id < 5000
+      WHERE cobranded_application_id < 5000;
 
 DELETE FROM onlineapp_cobranded_applications
-      WHERE id < 5000
+      WHERE id < 5000;
+
+END;
