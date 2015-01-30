f184d2d0b591e996e491436c2d38ab18a749ba9d
Sean Brady <sbrady@axiapayments.com>
2015-01-30 12:54:26 -0800

    sql changes

--- app/Config/Migration/Utils/delete_migrated_apps.sql
+++ app/Config/Migration/Utils/delete_migrated_apps.sql
@@ -1,16 +1,12 @@
 BEGIN;
 
-UPDATE onlineapp_template_fields
-   SET merge_field_name = 'OwnInventory-'
- WHERE merge_field_name ilike 'OwnInventory-%';
-
 UPDATE onlineapp_coversheets
    SET cobranded_application_id = null
- WHERE onlineapp_application_id < 5000;
+ WHERE cobranded_application_id < 5000;
 
 UPDATE onlineapp_email_timelines
    SET cobranded_application_id = null
- WHERE app_id < 5000;
+ WHERE cobranded_application_id < 5000;
 
 DELETE FROM onlineapp_cobranded_application_values
       WHERE cobranded_application_id < 5000;
