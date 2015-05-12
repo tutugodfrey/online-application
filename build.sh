89591e68856c606beca1e3e5c46f6c3ed0d63ced
Seth Tanner <stanner@axiapayments.com>
2015-05-12 09:20:29 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 !#/bin/bash
-cd app
+mkdir app/tmp
+HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:${HTTPDUSER}:rwx app/tmp
+sudo setfacl -R -d -m u:${HTTPDUSER}:rwx app/tmp
+CURRENTUSER=`whoami`
+sudo setfacl -R -m u:${CURRENTUSER}:rwx app/tmp
+app/Console/cake Migrations.migration run all
 Console/cake Migrations.migration run all
-php ../codecept.phar run CobrandedOnlineapp --steps
