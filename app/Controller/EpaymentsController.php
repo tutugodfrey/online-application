81b9e0840a7615067e9667ca35d67c0f970df465
Oscar Mota <omota@axiapayments.com>
2017-03-29 15:45:30 -0700

    removed magic strings

--- app/Controller/EpaymentsController.php
+++ app/Controller/EpaymentsController.php
@@ -1,208 +1,107 @@
 <?php
 class EpaymentsController extends AppController {
-    
-    //public $components = array ('RequestHandler');
-       public $components = array('RequestHandler','Security');
-    public $scaffold = 'admin';
-    
-        
-    function beforeFilter() {
-        parent::beforeFilter();
-        if ($this->_isJSON() && $this->request->is('get')) {
-            $this->Security->unlockedActions= array('api_sourcekey');
-        }
-    }
-//        if ($this->_isJSON() && 0 < count($this->Epayment->User->Apip->find('list', array('fields' => array('Apip.ip_address'), 'conditions' => array('Apip.ip_address >>=' => $this->RequestHandler->getClientIP()))))) {
-//            $this->Auth->allow(array('api_sourcekey'));
-//            $this->Security->loginOptions = array(
-//                'type' => 'basic',
-//                'realm' => 'My REST services,services',
-//                'login' => '_restLogin'
-//            );
-//
-//            $this->Security->requireLogin(array('api_sourcekey'));
-//            $this->Security->validatePost = false;
-//            if (
-//                    $this->_isJSON() &&
-//                    !$this->RequestHandler->isGet()
-//            ) {
-//                ///end New code added 02/23/2012
-//                if (empty($this->request->data) && !empty($_POST)) {
-//                    $this->request->data[$this->modelClass] = $_POST;
-//                }
-//            }
-//        }
-//    }
 
-    //New code added omm
-    public function _restLogin($credentials) {
+	//public $components = array ('RequestHandler');
+	public $components = array('RequestHandler','Security');
+	public $scaffold = 'admin';
 
-        $model = $this->Auth->getModel();
-        try {
-            $id = $model->useToken($credentials['username']);
-            if (empty($id)) {
-                $this->redirect(null, 503);
-            }
-        } catch (Exception $e) {
-            $id = null;
-        }
-        if (empty($id) || !$this->Auth->login(strval($id))) {
-            $this->Security->blackhole($this, 'login');
-        }
-    }//--end New code added 02/23/12
+	public function beforeFilter() {
+		parent::beforeFilter();
+		if ($this->_isJSON() && $this->request->is('get')) {
+			$this->Security->unlockedActions= array('api_sourcekey');
+		}
+	}
 
-    public function beforeRender() {
-        parent::beforeRender();
-        if ($this->_isJSON()) {
-            Configure::write('debug', 0);
-            $this->disableCache();
-        }
-    }
-            protected function _isJSON() {
-            return ($this->params['ext'] == 'json' || $this->request->accepts('application/json'));
-        }
-// ******  ADD, EDIT, INDEX and DELETE for FUTURE USE ******  //
-        /*
-    public function api_add(){
-        $this->setAction('edit');
-    }
+	//New code added omm
+	public function _restLogin($credentials) {
 
-    public function api_edit($id=null) {
-        if ($this->_isJSON() && !$this->RequestHandler->isPost()) {
-            $this->redirect(null, 400);
-        }
-        if ($this->Common->isPosted()) {
-            if (!empty($id)) {
-                $this->Epayment->id = $id;
-            } else {
-                $this->Epayment->create();
-            }
-            if ($this->Epayment->save($this->request->data)) {
-                $this->Session->setFlash('Epayment Item Added');
-                if ($this->_isJSON()) {
-                    $this->redirect(null, 200);
-                } else {
-                $this->redirect(array('action' => 'index'));
-                }
-            } else {
-                if ($this->_isJSON()) {
-                    $this->redirect(null,403);
-                } else {
-                    $this->Session->setFlash('Please Fix It');
-                }
-                
-            }
-        } elseif(!empty($id)) {
-            $this->request->data = $this->Epayment->find('first', array(
-                'conditions' => array('Epayment.id' => $id)
-            ));
-            if (empty($this->request->data)) {
-                throw new NotFoundException();
-            }
-        }
-        $this->set(compact('id'));
-    }
-    
-    public function api_delete($id) {
-        if ($this->_isJSON() && !$this->RequestHandler->isDelete()) {
-            $this->redirect(null, 400);
-        }
-        $epayment = $this->Epayment->find('first', array(
-            'conditions' => array('Epayment.id' => $id)
-        ));
-        if (empty($this->request->data)) {
-            if ($this->_isJSON()) {
-                $this->redirect(null, 404);
-            }
-            throw new NotFoundException();
-        }
-        if (!empty($this->request->data) || $this->RequestHandler->isDelete()) {
-            if ($this->Epayment->delete($id)) {
-                $this->Session->SetFlash ('Epayment deleted');
-                $this->redirect(array('action' => 'index'));
-                if ($this->_isJSON()) {
-                    $this->redirect(null, 200);
-                } else {
-                    $this->redirect(array('action'=>'index'));
-                }
-            } else {
-                if ($this->_isJSON()) {
-                    $this->redirect(null, 403);
-                } else {
-                    $this->Session->SetFlash('Could not delete Epayment');
-                }
-            }
-        }
-        $this->set(compact('epayment'));
-            
-    }
+		$model = $this->Auth->getModel();
+		try {
+			$id = $model->useToken($credentials['username']);
+			if (empty($id)) {
+				$this->redirect(null, 503);
+			}
+		} catch (Exception $e) {
+			$id = null;
+		}
+		if (empty($id) || !$this->Auth->login(strval($id))) {
+			$this->Security->blackhole($this, 'login');
+		}
+	}//--end New code added 02/23/12
 
-    public function api_index($id) {
-        
-        if ($this->_isJSON() && !$this->RequestHandler->isGet()) {
-            $this->redirect(null, 400);
-        }
-        
-        $epayment = $this->Epayment->find('all', array('fields' => array('Epayment.merchant_id','Epayment.pin')));
-        
-        $key = array(array('key' => 'key'));
-        $epayments = array_merge($epayment,$key);
-        //$this->set('var', 'vars');
-        $this->set(compact('epayments'));
-    }
-     * 
-     */
+	public function beforeRender() {
+		parent::beforeRender();
+		if ($this->_isJSON()) {
+			Configure::write('debug', 0);
+			$this->disableCache();
+		}
+	}
 
-    public function api_sourcekey() {
-        if ($this->_isJSON() && !$this->request->is('get')) {
-            $this->redirect(null, 400);
-        }
-        $epayments = $this->Epayment->find('all', array('conditions' => array('Epayment.merchant_id' => $this->request->query['merchant_id']), 'fields' => array('Epayment.pin', 'Epayment.application_id')));
-        $epayments[0]['Epayment']['sourcekey'] = trim($this->getSourceKey($epayments[0]['Epayment']['application_id']));
-        unset ($epayments[0]['Epayment']['application_id']);
-        if($epayments[0]['Epayment']['sourcekey'] != '') {
-	$data = $this->Epayment->findByMerchantId($this->request->query['merchant_id']);
-        $this->Epayment->id = $data['Epayment']['id'];
-        $this->Epayment->saveField('date_retrieved',DboSource::expression('NOW()'));
-            
-        }
-        $this->set(compact('epayments'));
-    }
-    
-    protected function getSourceKey($appId) {
-        $apikey = '72063deb919178dd02646d543bb58da3';
+	protected function _isJSON() {
+			return ($this->params['ext'] == 'json' || $this->request->accepts('application/json'));
+		}
 
-// API Pin assigned by USAePay
-        $pin = 'f4c31e4ed207c0ce';
+	public function api_sourcekey() {
+		if ($this->_isJSON() && !$this->request->is('get')) {
+			$this->redirect(null, 400);
+		}
+		$epayments = $this->Epayment->find('all', array('conditions' => array('Epayment.merchant_id' => $this->request->query['merchant_id']), 'fields' => array('Epayment.pin', 'Epayment.application_id')));
+		$epayments[0]['Epayment']['sourcekey'] = trim($this->getSourceKey($epayments[0]['Epayment']['application_id']));
+		unset ($epayments[0]['Epayment']['application_id']);
+		if ($epayments[0]['Epayment']['sourcekey'] != '') {
+		$data = $this->Epayment->findByMerchantId($this->request->query['merchant_id']);
+		$this->Epayment->id = $data['Epayment']['id'];
+		$this->Epayment->saveField('date_retrieved', DboSource::expression('NOW()'));
 
-// Generate a random salt
-        $salt = mt_rand();
+		}
+		$this->set(compact('epayments'));
+	}
 
-// Hash the salt using the pin
-        $hash = hash_hmac('sha256', $salt, $pin, true);
+	protected function getSourceKey($appId) {
+		$apikey = Epayment::API_KEY;
 
-// Base64 Encode the Hash
-        $hash = base64_encode($hash);
+		// API Pin assigned by USAePay
+		$pin = Epayment::API_PIN;
 
-// Raw URL Encode the Hash
-        $hash = rawurlencode($hash);
+		// Generate a random salt
+		$salt = mt_rand();
 
-// Concatinate the salt and hash
-        $hash = '$s256$' . $salt . '$' . $hash;
+		// Hash the salt using the pin
+		$hash = hash_hmac('sha256', $salt, $pin, true);
 
-// Append the apikey and hash values to the request url
-        $url = 'https://secure.axiaepay.com/interface/vendorapi/sourcekeys?output=xml&apikey=' . $apikey . '&hash=' . $hash . '&startdate=2012-05-07';
-        //echo $url;
-// Send Request
-        $xml = new SimpleXMLElement(file_get_contents($url));
-        //print_r($xml); exit;
-        foreach ($xml->SourceKeys->SourceKey as $s) {
-        if ($s->ApplicationID == $appId) {
-            return $s->SourceKey; 
-        }
-            
-        } return false;
-    }
-    
+		// Base64 Encode the Hash
+		$hash = base64_encode($hash);
+
+		// Raw URL Encode the Hash
+		$hash = rawurlencode($hash);
+
+		// Concatinate the salt and hash
+		$hash = '$s256$' . $salt . '$' . $hash;
+
+		// Append the apikey and hash values to the request url
+		$url = Epayment::API_REQ_URL . $apikey . '&hash=' . $hash . '&startdate=' . Epayment::API_START_DATE;
+		//echo $url;
+		// Send Request
+		$xml = new SimpleXMLElement(file_get_contents($url));
+		//print_r($xml); exit;
+		foreach ($xml->SourceKeys->SourceKey as $s) {
+		if ($s->ApplicationID == $appId) {
+			return $s->SourceKey;
+		}
+
+		} return false;
+	}
+
+/**
+ * Create admin prefix Index method
+ *
+ * @return null
+ */
+	public function admin_index() {
+		$this->paginate = array(
+			'contain' => array('User' => array('fields' => array('id', 'fullname')))
+		);
+		$this->set('epayments', $this->paginate());
+	}
 }
 ?>
--- app/Model/Epayment.php
+++ app/Model/Epayment.php
@@ -1,15 +1,33 @@
 <?php
 class Epayment extends AppModel {
-       public $useTable = 'onlineapp_epayments';
-    
-    public $belongsTo = array(
-        'User' => array(
-            'className' => 'User',
-            'foreignKey' => 'user_id',
-            'conditions' => '',
-            'fields' => '',
-            'order' => ''
-        )
-    );
+/**
+ * API KEY
+ */
+	const API_KEY = '72063deb919178dd02646d543bb58da3';
+/**
+ * API PIN
+ */
+	const API_PIN = 'f4c31e4ed207c0ce';
+
+/**
+ * Api start date
+ */
+	const API_START_DATE = '2012-05-07';
+
+/**
+ * Api request url 
+ */
+	const API_REQ_URL = 'https://secure.axiaepay.com/interface/vendorapi/sourcekeys?output=xml&apikey=';
+
+	public $useTable = 'onlineapp_epayments';
+
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
-?>
