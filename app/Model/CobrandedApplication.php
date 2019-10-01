a1a051f1a1bf2aa7033f6f798ffc9e8f4fb85dda
Oscar Mota <omota@axiapayments.com>
2019-10-01 09:29:48 -0700

    minor change to substing search function to use

--- app/Model/CobrandedApplication.php
+++ app/Model/CobrandedApplication.php
@@ -1767,7 +1767,7 @@ class CobrandedApplication extends AppModel {
 		$template = 'rep_notify_signed';
 		
 		$description = "Application Description: ";
-		if (strripos(Hash::get($cobrandedApplication, 'Template.name'), 'Cancellation')) {
+		if (stripos(Hash::get($cobrandedApplication, 'Template.name'), 'Cancellation')) {
 			$description = 'Description: ';
 			$template = 'rep_notify_cancellation_signed';
 			$to[] = EmailTimeline::CANCELLATIONS_EMAIL;
