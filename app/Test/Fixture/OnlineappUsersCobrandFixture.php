f88578e514d66cb30135dfa2e578c58aa513f004
Oscar Mota <omota@axiapayments.com>
2019-10-15 11:06:09 -0700

    added table schema definition for onlineapp_user_cobrands

--- app/Test/Fixture/OnlineappUsersCobrandFixture.php
+++ app/Test/Fixture/OnlineappUsersCobrandFixture.php
@@ -1,6 +1,27 @@
 <?php
 class OnlineappUsersCobrandFixture extends CakeTestFixture {
 
-	public $import = 'UsersCobrand';
+	// public $import = 'UsersCobrand';
+
+/**
+ * Table name
+ *
+ * @var string
+ */
+	public $table = 'onlineapp_users_cobrands';
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false),
+		'cobrand_id' => array('type' => 'integer', 'null' => false),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
 
 }
