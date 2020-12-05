c44cfba713fe5104431a0b9fcbe58877bae8f5b7
omota <omota@axiamed.com>
2020-12-05 11:42:31 -0800

    updated migration and added some images for future use

--- app/Config/Migration/1604342075_create_clients_fields.php
+++ app/Config/Migration/1604342075_create_clients_fields.php
@@ -50,23 +50,4 @@ class CreateClientsFields extends CakeMigration {
 			)
 		),
 	);
-
-/**
- * After migration callback
- *
- * @param string $direction Direction of migration process (up or down)
- * @return bool Should process continue
- */
-	public function after($direction) {
-		if ($direction == 'up') {
-			$Template = ClassRegistry::init('Template');
-			//Back up old template guids
-
-			$Template->updateAll(
-				['require_client_data' => true],
-				['id' => array(168,195,163,130,162,174,172,173,183,187,135,157,129,155,177,134,118,170,124,156,127,113,110,120,179,192)]
-			);
-		}
-		return true;
-	}
 }
