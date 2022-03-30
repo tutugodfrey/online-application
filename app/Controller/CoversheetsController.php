b8f1acd650acf2dcb910cdaa2e79ff1c3756c28f
Oscar Mota <omota@axiamed.com>
2022-03-30 15:19:09 -0700

    fixed issue with coversheet partners dropdown

--- app/Controller/CoversheetsController.php
+++ app/Controller/CoversheetsController.php
@@ -130,7 +130,7 @@ class CoversheetsController extends AppController {
         }
         $dbUserAssocParters = [];
         if (Hash::get($data, 'Coversheet.status') === 'saved') {
-        	$requestUri = '/api/Users/get_reps?'. http_build_query(["user_name" => Hash::get($data, 'ContractorID')], "", null, PHP_QUERY_RFC3986);
+        	$requestUri = '/api/Users/get_reps?'. http_build_query(["user_name" => Hash::get($data, 'CobrandedApplication.ContractorID')], "", null, PHP_QUERY_RFC3986);
 	        $axDbApiClient = $this->Coversheet->createAxiaDbApiAuthClient('GET', $requestUri);
 			$reponse = $axDbApiClient->get('https://db.axiatech.com'.$requestUri);
 			$responseData = json_decode($reponse->body, true);
